6dd1ff5b7f030078c46e9b6b397f6291bc5ac45e
Abigael Haidar <abigaelhaidar@gmail.com>
2025-09-03 22:28:27 +0700

    membuat navbar

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum
+        ipsum atque, quasi nisi quis eaque enim architecto. Voluptates, quaerat!
+        Magnam, quod error numquam corrupti unde inventore! Molestias
+        repellendus veniam, voluptate laudantium saepe veritatis! Libero
+        voluptatem dolores earum ipsam minus beatae nostrum quis, deleniti,
+        molestias quod officiis aliquam inventore amet cupiditate. Atque harum
+        dolor expedita maiores amet ullam dolores quisquam consectetur libero
+        repellendus dolorem enim iure hic, perferendis culpa ex nulla omnis
+        assumenda voluptate id laborum quia nobis, aut fugit. Maxime et libero
+        necessitatibus nam nihil minus minima eveniet sit quibusdam nisi! Ullam
+        repellat cum iure animi error asperiores delectus ut atque accusamus
+        dicta commodi explicabo quam veniam numquam eveniet aliquid sunt,
+        assumenda magnam similique eligendi perspiciatis ex corrupti. Nisi at
+        ad, magnam neque ipsam laudantium fugiat, reprehenderit earum animi
+        omnis illo nesciunt aut accusantium facere saepe minus. Deserunt optio
+        excepturi amet, voluptatibus obcaecati autem dicta perferendis molestiae
+        dolor blanditiis culpa quia consectetur praesentium ipsam. Ullam
+        deleniti quaerat amet odit perspiciatis laudantium sunt, aliquid
+        repellat numquam rerum, similique rem veritatis veniam soluta expedita
+        mollitia debitis, omnis tenetur beatae autem. Tempora adipisci vitae
+        eligendi excepturi doloribus quia nisi quis quas ipsa repellendus
+        placeat iste dolores ex pariatur consequuntur nihil, enim nobis!
+      </h1>
+    </>
+  );
+}
+
+export default App;
