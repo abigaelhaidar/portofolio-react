e45c18f66fd219c250c80fd65c7b9cd936f7e0f0
Abigael Haidar <abigaelhaidar@gmail.com>
2025-09-04 16:08:10 +0700

    membuat loader

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
+import Preloader from "./components/Preloader.jsx";
 
 import 'remixicon/fonts/remixicon.css'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Preloader />
     <div className="container mx-auto px-6">
       <Navbar />
       <App />
