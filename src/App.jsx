10c49554da4e04ec95eeacfd5ba6b9ecabc83b01
Abigael Haidar <abigaelhaidar@gmail.com>
2025-09-04 13:11:39 +0700

    membuat tentang section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import DataImage from "./data"; // import data image
+import { listTools } from "./data"; // import data tools
 
 function App() {
   return (
     <>
+      {/* Hero Section / Beranda */}
       <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
         <div>
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
@@ -44,6 +46,71 @@ function App() {
           className="w-[500px] md:ml-auto"
         />
       </div>
+      {/* Akhir Hero Section / Beranda */}
+
+      {/* Awal Tentang Section */}
+      <div className="tentang mt-32 py-10">
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+          <img
+            src={DataImage.HeroImage}
+            className="w-12 rounded-md mb-10 sm:hidden"
+            alt=""
+          />
+          <p className="text-base/loose mb-10">
+            Hi, perkenalkan saya Abigael Haidar, seorang Full Stack Web
+            Developer dan Designer untuk UI/UX Design maupun Product Digital,
+            Saya percaya bahwa desain dan fungsionalitas harus berjalan
+            beriringan, sehingga setiap proyek yang saya kembangkan tidak hanya
+            terlihat menarik tetapi juga memberikan pengalaman pengguna yang
+            optimal.
+          </p>
+          <div className="flex items-center justify-between">
+            <img
+              src={DataImage.HeroImage}
+              alt="Hero Image"
+              className="w-12 rounded-md sm:block hidden"
+            />
+            <div className="flex items-center gap-6">
+              <div>
+                <h1 className="text-4xl mb-1">
+                  45<span className="text-violet-500">+</span>
+                </h1>
+                <p>Project Selesai</p>
+              </div>
+              <div>
+                <h1 className="text-4xl mb-1">
+                  4<span className="text-violet-500">+</span>
+                </h1>
+                <p>Tahun Pengalaman</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="tools mt-32">
+          <h1 className="text-4xl/snug font-bold mb-4">Tools yang dipakai</h1>
+          <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">
+            Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan
+            Website ataupun Design
+          </p>
+          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+            {listTools.map((tool) => (
+              <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group" key={tool.id}>
+                <img
+                  src={tool.gambar}
+                  alt="Tools Image"
+                  className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
+                />
+                <div>
+                  <h4 className="font-bold">{tool.nama}</h4>
+                  <p className="opacity-50">{tool.ket}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {/* Akhir Tentang Section */}
     </>
   );
 }
