0040facf4f9c2651a6a4d048e866e0f18bb176e7
Abigael Haidar <abigaelhaidar@gmail.com>
2025-09-04 10:46:20 +0700

    membuat hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
+import DataImage from "./data"; // import data image
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum
-        ipsum atque, quasi nisi quis eaque enim architecto. Voluptates, quaerat!
-        Magnam, quod error numquam corrupti unde inventore! Molestias
-        repellendus veniam, voluptate laudantium saepe veritatis! Libero
-        voluptatem dolores earum ipsam minus beatae nostrum quis, deleniti,
-        molestias quod officiis aliquam inventore amet cupiditate. Atque harum
-        dolor expedita maiores amet ullam dolores quisquam consectetur libero
-        repellendus dolorem enim iure hic, perferendis culpa ex nulla omnis
-        assumenda voluptate id laborum quia nobis, aut fugit. Maxime et libero
-        necessitatibus nam nihil minus minima eveniet sit quibusdam nisi! Ullam
-        repellat cum iure animi error asperiores delectus ut atque accusamus
-        dicta commodi explicabo quam veniam numquam eveniet aliquid sunt,
-        assumenda magnam similique eligendi perspiciatis ex corrupti. Nisi at
-        ad, magnam neque ipsam laudantium fugiat, reprehenderit earum animi
-        omnis illo nesciunt aut accusantium facere saepe minus. Deserunt optio
-        excepturi amet, voluptatibus obcaecati autem dicta perferendis molestiae
-        dolor blanditiis culpa quia consectetur praesentium ipsam. Ullam
-        deleniti quaerat amet odit perspiciatis laudantium sunt, aliquid
-        repellat numquam rerum, similique rem veritatis veniam soluta expedita
-        mollitia debitis, omnis tenetur beatae autem. Tempora adipisci vitae
-        eligendi excepturi doloribus quia nisi quis quas ipsa repellendus
-        placeat iste dolores ex pariatur consequuntur nihil, enim nobis!
-      </h1>
+      <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
+        <div>
+          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
+            <img
+              src={DataImage.HeroImage}
+              alt="Hero Image"
+              className="w-10 rounded-md"
+            />
+            <q>Kode yang indah, lahir dari kebiasaan dan ketekunan.😎</q>
+          </div>
+          <h1 className="text-5xl/tight font-bold mb-6">
+            Hi, saya Abigael Haidar
+          </h1>
+          <p className="text-base/loose mb-6 opacity-50">
+            Saya mahasiswa Informatika dengan minat di pengembangan web frontend
+            dan backend. Berpengalaman 4 tahun menguasai HTML, CSS, JavaScript,
+            serta framework Bootstrap 5 dan Tailwind CSS. Terbiasa mengelola
+            data dari API dan mengembangkan PWA untuk performa, aksesibilitas,
+            serta pengalaman pengguna yang lebih baik.
+          </p>
+          <div className="flex items-center sm:gap-4 gap-2">
+            <a
+              href="#"
+              className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600"
+            >
+              Download CV <i className="ri-download-line ri-lg"></i>
+            </a>
+            <a
+              href="#"
+              className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600"
+            >
+              Lihat Project <i className="ri-arrow-down-line ri-lg"></i>
+            </a>
+          </div>
+        </div>
+        <img
+          src={DataImage.HeroImage}
+          alt="Hero Image"
+          className="w-[500px] md:ml-auto"
+        />
+      </div>
     </>
   );
 }
